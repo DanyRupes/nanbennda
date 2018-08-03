daed2536b5866186a2f8e30169b157b5ca2f5f64
Danyrupes <danyrupes.1505026@srec.ac.in>
2018-08-04 05:03:24 +0530

    nun

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express()
 const fs = require('fs')
 const path = require('path')
 const router = require('./Router/router.js')
-const port = 443;
+const port = 80;
 app.use('/', router)
 // console.log(path.join(__dirname,router))
 const https = require('https')
@@ -22,9 +22,10 @@ const httpsOptions = {
     key : fs.readFileSync(path.join(__dirname, 'certificates','private.pem')),
     cert : fs.readFileSync(path.join(__dirname, 'certificates','certificate.crt')),
 }
-https.createServer(httpsOptions, app)
-.listen(port, function () { 
-    console.log(port)
+https.createServer(httpsOptions,app)
+.listen(443, function () { 
+    console.log("443")
  })
 
+app.listen(port,()=>{console.log(port)})
 
